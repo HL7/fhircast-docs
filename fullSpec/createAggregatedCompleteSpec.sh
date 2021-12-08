8ef700f9bc01c9e98408aab64202fad4cdb584ab
Isaac Vetter <isaac@epic.com>
2021-12-08 16:48:28 -0600

    add fullSpec

---
+++ fullSpec/createAggregatedCompleteSpec.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# copies all sections to a single markdown file and adds it to the site so it can be build
+# this is usefull for review.
+
+sushiconfig="../sushi-config.yaml"
+fullspec="fullspec.md"
+
+rm -Rf $fullspec
+rm -Rf $fullspec.tmp
+
+echo "#" FHIRcast > $fullspec
+echo >> FHIRcast.md
+
+echo 1. The generated Toc will be an ordered list >> $fullspec
+echo {:toc} >> $fullspec
+echo >> $fullspec
+
+cat $sushiconfig | grep -e .md -e title: | while read line1
+do
+    read line2
+    # echo title: $line2
+    # echo md: $line1
+
+    echo "##" ${line2#title:} >> $fullspec
+    echo >> $fullspec
+    fn=../input/pagecontent/${line1%":"}
+    cat $fn >> $fullspec
+done
+
+mv $fullspec ../temp/pages/_includes
+cp -f fullspec-md.html ../temp/pages/_includes
+cp fullspec.html ../temp/pages
+cd ../temp/pages
+
+jekyll b
+
+# cd output
+# mv ../$fullspec .
+# kramdown --auto-ids $fullspec > FHIRcast.html
+
+# <img align="left" width="100%"  src="colorful%20overview%20diagram.png">
+# {% include img.html img="events-railroad.png" caption="Figure: Syntax for new events" %}
